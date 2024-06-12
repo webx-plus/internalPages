87f6c4bea2228d633c0b6af0ef33cc772c60922d
lifeisfakenews <lifeisfakenews69@gmail.com>
2024-06-12 17:18:03 +0100

    Wonder if this will work first time

--- history.lua
+++ history.lua
@@ -5,7 +5,17 @@ print(history)
 
 local history_map = history.map(function(item)
     -- return "<div><a href=\"" .. item.url "\"><p>" .. item.name .. "</p><p>" .. item.url .. "</p><p>" .. item.date .. "</p></a></div>"
-    return "<div>" .. item.name .. "</div>"
+    local date = os.date("%x") == os.date("%x", item.date) and os.date("%H:%M", item.date) or os.date("%d/%m/%y", item.date)
+    local string = [[
+        <div class="history-item">
+            <a href="%s">
+                <p>%s</p>
+                <p>%s</p>
+                <p>%s</p>
+            </a>
+        </div>
+    ]]
+    return string.format(string, item.url, item.name, item.url, date)
 end)
 
 history_list.set_content(history_map)
