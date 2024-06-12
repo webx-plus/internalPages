fde18d1a9dd5cf42d7217eea56c70140897f1f4f
lifeisfakenews <lifeisfakenews69@gmail.com>
2024-06-12 16:46:52 +0100

    did a thing maybe

--- history.lua
+++ history.lua
@@ -4,7 +4,7 @@ local history = __bussinga.get_history()
 print(history)
 
 local history_map = history_list.map(function(item)
-    return `<div><a href=\"` .. item.url `"><p>` .. item.title .. `</p><p>` .. item.url .. `</p><p>` .. item.date .. `</p></a></div>`
+    return "<div><a href=\"" .. item.url "\"><p>" .. item.title .. "</p><p>" .. item.url .. "</p><p>" .. item.date .. "</p></a></div>"
 end)
 
 history_list.set_content(history_map)
