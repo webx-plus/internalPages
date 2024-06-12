14bfe8e4036abcb037c4d1315fbeb5cda39dcb82
lifeisfakenews <lifeisfakenews69@gmail.com>
2024-06-12 16:51:19 +0100

    h

--- history.lua
+++ history.lua
@@ -3,8 +3,8 @@ local history = __bussinga.get_history()
 
 print(history)
 
-local history_map = history_list.map(function(item)
-    return "<div><a href=\"" .. item.url "\"><p>" .. item.title .. "</p><p>" .. item.url .. "</p><p>" .. item.date .. "</p></a></div>"
-end)
+-- local history_map = history_list.map(function(item)
+--     return "<div><a href=\"" .. item.url "\"><p>" .. item.title .. "</p><p>" .. item.url .. "</p><p>" .. item.date .. "</p></a></div>"
+-- end)
 
-history_list.set_content(history_map)
+-- history_list.set_content(history_map)
