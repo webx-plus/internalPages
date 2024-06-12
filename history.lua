8d28cd3dcc1bb37b641d10f5b5253b9e7531a213
lifeisfakenews <lifeisfakenews69@gmail.com>
2024-06-12 16:30:39 +0100

    EEEE

--- history.lua
+++ history.lua
@@ -1,6 +1,6 @@
 local history_list = get("history")
-local history = __bussinga.getHistory()
+local history = __bussinga.get_history()
 
 print(history)
 
--- history_list.set_content(history)
+history_list.set_content(history)
