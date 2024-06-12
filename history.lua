6ded857dbfaa03418cd08dbd66970911a6f8d6cd
lifeisfakenews <lifeisfakenews69@gmail.com>
2024-06-12 16:57:16 +0100

    I am so stupid sometimes

--- history.lua
+++ history.lua
@@ -3,7 +3,7 @@ local history = __bussinga.get_history()
 
 print(history)
 
-local history_map = history_list.map(function(item)
+local history_map = history.map(function(item)
     -- return "<div><a href=\"" .. item.url "\"><p>" .. item.title .. "</p><p>" .. item.url .. "</p><p>" .. item.date .. "</p></a></div>"
     return item.title
 end)
